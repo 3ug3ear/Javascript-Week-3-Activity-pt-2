e5d5b000198eb057317cfc9b0bdc66d75c158220
3ug3ear <116048121+3ug3ear@users.noreply.github.com>
2022-11-02 21:01:09 -0700

    wrote the if else statments for A,B,C,D, and F

---
+++ script.js
@@ -0,0 +1,20 @@
+let myGrade =(50);
+console.log(myGrade);
+if (myGrade >= 90) {
+    console.log("I got an A!!! I'm the best!!!");
+}
+else if(myGrade < 90 && myGrade > 70) {
+    console.log("A B is fine as well i suppose.");
+}
+else if(myGrade < 80 && myGrade > 60) {
+    console.log("Really, a C?");
+} 
+else if(myGrade < 70 && myGrade > 50) {
+    console.log("D. just go home.");
+}
+else if(myGrade < 60 && myGrade > 40) {
+    console.log("FAILURE!")
+}
+else {
+    console.log("Oops. Better study more.");
+}
